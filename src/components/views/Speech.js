6ad991f4e9ac3d3fd56775f2b1b850add5c07408
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-10-20 23:38:21 +0200

    push

--- src/components/views/Speech.js
+++ src/components/views/Speech.js
@@ -1,50 +1,82 @@
-import { speech } from "@google-cloud/speech";
-
+// import { speech } from "@google-cloud/speech";
+const querystring = require("querystring");
+const https = require("https");
 export var transcribe = function(file) {
   return new Promise(function(resolve, reject) {
+    var postData = querystring.stringify({
+      audio: {}
+    });
+
+    var options = {
+      hostname:
+        "https://speech.googleapis.com/v1/speech:recognize?key=AIzaSyBJjGtJPYIaZLKMNOmNX-051wiCYLcw6TU",
+      port: 443,
+      // path: "/post.php",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": postData.length
+      }
+    };
+
+    var req = https.request(options, res => {
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+
+      res.on("data", d => {
+        process.stdout.write(d);
+      });
+    });
+
+    req.on("error", e => {
+      console.error(e);
+    });
+
+    req.write(postData);
+    req.end();
+
     // [START speech_transcribe_sync_gcs]
-    // Imports the Google Cloud client library
 
     // Creates a client
-    const client = new speech.SpeechClient();
-    const gcsUri = file;
-    const encoding = "Eencoding of the audio file, e.g. LINEAR16";
-    const sampleRateHertz = 16000;
-    const languageCode = "BCP-47 language code, e.g. en-US";
+    // const client = new speech.SpeechClient();
+    // const gcsUri = file;
+    // const encoding = "Eencoding of the audio file, e.g. LINEAR16";
+    // const sampleRateHertz = 16000;
+    // const languageCode = "BCP-47 language code, e.g. en-US";
 
-    const config = {
-      encoding: encoding,
-      sampleRateHertz: sampleRateHertz,
-      languageCode: languageCode
-    };
-    const audio = {
-      uri: gcsUri
-    };
+    // const config = {
+    //   encoding: encoding,
+    //   sampleRateHertz: sampleRateHertz,
+    //   languageCode: languageCode
+    // };
+    // const audio = {
+    //   uri: gcsUri
+    // };
 
-    const request = {
-      config: config,
-      audio: file
-    };
+    // const request = {
+    //   config: config,
+    //   audio: file
+    // };
 
-    // Detects speech in the audio file
-    client
-      .recognize(request)
-      .then(data => {
-        const response = data[0];
-        const transcription = response.results
-          .map(result => result.alternatives[0].transcript)
-          .join("\n");
-        console.log(`Transcription: `, transcription);
-        var transcriptInfo = {};
-        transcriptInfo.skills = analyseTranscript(transcription);
-        console.log(`Skills: `, transcriptInfo.skills);
-        var jsonText = JSON.stringify(transcriptInfo);
-        document.writeln(jsonText);
-        resolve(transcription);
-      })
-      .catch(err => {
-        reject(err);
-      });
+    // // Detects speech in the audio file
+    // client
+    //   .recognize(request)
+    //   .then(data => {
+    //     const response = data[0];
+    //     const transcription = response.results
+    //       .map(result => result.alternatives[0].transcript)
+    //       .join("\n");
+    //     console.log(`Transcription: `, transcription);
+    //     var transcriptInfo = {};
+    //     transcriptInfo.skills = analyseTranscript(transcription);
+    //     console.log(`Skills: `, transcriptInfo.skills);
+    //     var jsonText = JSON.stringify(transcriptInfo);
+    //     document.writeln(jsonText);
+    //     resolve(transcription);
+    //   })
+    //   .catch(err => {
+    //     reject(err);
+    //   });
   });
 };
 
