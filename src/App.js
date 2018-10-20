c9617b693081dd7da5c4329adfc7e99d7e275f43
Armin Windlin <armin.windlin@swisscom.com>
2018-10-20 11:52:51 +0200

    video example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import VideoExample from './components/VideoExample';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/test';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Test/>
+                <div>
+                    <h1>snapCV</h1>
+                    <hr />
+                    <VideoExample />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
