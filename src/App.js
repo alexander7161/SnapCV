0da884580ff0bf104bd79665d6fddf32de4f36d9
Laquesisa <dana.shmaria@gmail.com>
2018-10-20 16:21:28 +0200

    Implemented Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import Navbar from "./components/Navbar";
+import { Redirect } from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <Navbar routes={this.routes}/>
+            <Navbar />
         );
     }
 }
