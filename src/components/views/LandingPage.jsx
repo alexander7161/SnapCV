8cbd8c9585e4f46959a5a5f821574d8cf1920ab3
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-10-21 05:30:09 +0200

    Centre

--- src/components/views/LandingPage.jsx
+++ src/components/views/LandingPage.jsx
@@ -1,22 +1,48 @@
-import React, {Component} from 'react';
-import '../../stylesheets/views/LandingPage.css';
-import mainLogo from'./InfoJobs.png';
-
+import React, { Component } from "react";
+import "../../stylesheets/views/LandingPage.css";
+import mainLogo from "./InfoJobs.png";
 
 class LandingPage extends Component {
-    render() {
-        return (
-            <div>
-			  <p style={{'fontSize': '18px', display: 'flex', justifyContent: 'center'}}>This is a project that transforms the job registration process.</p>
-			  <p style={{'fontSize': '18px', display: 'flex', justifyContent: 'center'}}>With a simple video recording, your application information will be smartly recorded and be sent to InfoJobs for automatic job matching.</p>
-			  <p style={{'fontSize': '30px', display: 'flex', justifyContent: 'center'}}>Get ready to be hired!</p>
-			  <img style={{width: 1270, height: 220}} src={mainLogo}></img>
-
-
-			 </div>
-        );
-    }
-	
+  render() {
+    return (
+      <div>
+        <p
+          style={{
+            fontSize: "18px",
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          This is a project that transforms the job registration process.
+        </p>
+        <p
+          style={{
+            fontSize: "18px",
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          With a simple video recording, your application information will be
+          smartly recorded and be sent to InfoJobs for automatic job matching.
+        </p>
+        <p
+          style={{
+            fontSize: "30px",
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          Get ready to be hired!
+        </p>
+        <div style={{ display: "flex" }}>
+          <img
+            style={{ width: 1270, height: 220, margin: "0 auto" }}
+            src={mainLogo}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
